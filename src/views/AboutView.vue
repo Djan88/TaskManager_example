bafa59b5f60d473d55ef66561dd950330ad05fb1
Ivan Avraamov <i.avraamov@idaproject.com>
2022-08-16 10:58:45 +0300

    New page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>Stay in touch...</h1>
+    <h1>Тестирование</h1>
+    <div class="container">
+      <div class="container_item container_item-1">1</div>
+      <div class="container_item">2</div>
+      <div class="container_item">3</div>
+      <div class="container_item">4</div>
+      <div class="container_item">5</div>
+      <div class="container_item">6</div>
+      <div class="container_item">7</div>
+      <div class="container_item">8</div>
+      <div class="container_item">9</div>
+    </div>
   </div>
 </template>
+<style scoped>
+.about {}
+.container{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: repeat(3, 100px);
+  gap: 10px;
+}
+.container_item {
+  background: #eee;
+  border: 1px solid green;
+}
+.container_item:nth-child(odd){
+  background: #666;
+  color: #fff;
+}
+.container_item-1
+{
+  grid-column: 1 / 4;
+}
+</style>
